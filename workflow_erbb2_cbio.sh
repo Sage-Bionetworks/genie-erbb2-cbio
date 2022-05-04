35585198b25d75c3e80ad285c8912575fbd381cc
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-05-04 01:37:28 +0000

    reorganize files and create new synapse entities

--- workflow_erbb2_cbio.sh
+++ workflow_erbb2_cbio.sh
@@ -2,7 +2,6 @@
 # Author: Haley Hunter-Zinck
 # Date: 2022-05-03
 
-Rscript uncode_redcap_export.R -e syn29875992 -d syn29880018 -o syn8322425 -f genie_erbb2_uncoded_export.csv -v
-#Rscript create_clinical.R -i syn29990375 -m syn29989712 -o syn8363326 -v 
-Rscript create_clinical.R -i syn29990375 -m syn29989712 -v 
+Rscript uncode_redcap_export.R -e syn30042048 -d syn29880018 -o syn30041935 -f genie_erbb2_uncoded_export.csv -v
+Rscript create_clinical.R -i syn29990375 -m syn29989712 -o syn30041961 -v 
 
