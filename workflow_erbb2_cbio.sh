293a42eea4b463425fd0ab56e40bef3d753c17ad
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-05-04 00:26:09 +0000

    bash workflow

---
+++ workflow_erbb2_cbio.sh
@@ -0,0 +1,8 @@
+# Description: Workflow for regenerating ERBB2 cBioPortal files.
+# Author: Haley Hunter-Zinck
+# Date: 2022-05-03
+
+Rscript uncode_redcap_export.R -e syn29875992 -d syn29880018 -o syn8322425 -f genie_erbb2_uncoded_export.csv -v
+#Rscript create_erbb2_cbio_clinical_files.R -i syn29990375 -m syn29989712 -o syn8363326 -v 
+Rscript create_erbb2_cbio_clinical_files.R -i syn29990375 -m syn29989712 -v 
+
