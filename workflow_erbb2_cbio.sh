dc9173e819ab6bbc72bcfa40c45ad9f415f09d59
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-05-06 03:48:49 +0000

    add genomic script

--- workflow_erbb2_cbio.sh
+++ workflow_erbb2_cbio.sh
@@ -3,5 +3,13 @@
 # Date: 2022-05-03
 
 Rscript uncode_redcap_export.R -e syn30042048 -d syn29880018 -o syn30041935 -f genie_erbb2_uncoded_export.csv -v
-Rscript create_clinical.R -i syn29990375 -m syn29989712 -o syn30041961 -v 
 
+Rscript create_clinical.R -i syn29990375 -m syn29989712 -o syn30041961 -v 
+Rscript create_genomic.R -i syn29990375 -g syn26706564 -o syn30041961 -v 
+
+#Rscript create_case.R
+#Rscript create_panel.R # filename <- "panel" = "data_gene_panel_{panel_name}.txt"
+
+# validate using cBioPortal validator
+
+# clean up local 
