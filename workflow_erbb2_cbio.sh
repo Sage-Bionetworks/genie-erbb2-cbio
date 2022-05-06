7bbde5024d91f3481d57ff66a8ca42d68482d6a8
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-05-06 05:19:57 +0000

    add cli for create case files script

--- workflow_erbb2_cbio.sh
+++ workflow_erbb2_cbio.sh
@@ -6,8 +6,8 @@ Rscript uncode_redcap_export.R -e syn30042048 -d syn29880018 -o syn30041935 -f g
 
 Rscript create_clinical.R -i syn29990375 -m syn29989712 -o syn30041961 -v 
 Rscript create_genomic.R -i syn29990375 -g syn26706564 -o syn30041961 -v 
+Rscript create_case.R -i syn30041961 -o syn30041961 -v
 
-#Rscript create_case.R
 #Rscript create_panel.R # filename <- "panel" = "data_gene_panel_{panel_name}.txt"
 
 # validate using cBioPortal validator
