74ec1c6d1592873983d61a1ab296efa0a4d35e09
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-05-13 23:47:55 +0000

    placeholder

--- create_meta.R
+++ create_meta.R
@@ -1,5 +1,6 @@
 # Description: create cBioPortal meta data files for each file type.  
 # Author: Haley Hunter-Zinck
+# Based on: https://github.com/Sage-Bionetworks/GENIE-Sponsored-Projects/blob/develop/geniesp/metafiles.py
 # Date: 2022-05-12
 
 # setup ----------------------------
@@ -49,36 +50,161 @@ auth <- opt$auth
 
 # functions ----------------------------
 
-create_meta_clinical <- function() {
+create_meta_clinical_generic <- function(cancer_study_identifier, genetic_alteration_type, datatype, data_filename) {
+  rows <- rep("", 4)
+  rows[1] <- c(glue("cancer_study_identifier: {cancer_study_identifier}"))
+  rows[2] <- c(glue("genetic_alteration_type: {genetic_alteration_type}"))
+  rows[3] <- c(glue("datatype: {datatype}"))
+  rows[4] <- c(glue("data_filename: {data_filename}"))
   return(NULL)
 }
 
-create_meta_genomic <- function() {
-  return(NULL)
+create_meta_genomic_generic <- function(cancer_study_identifier, genetic_alteration_type, datatype, stable_id, profile_name, profile_description, data_filename) {
+  
+  rows <- rep(NA, 8)
+  rows[1] <- glue("cancer_study_identifier: {cancer_study_identifier}")
+  rows[2] <- glue("genetic_alteration_type: {genetic_alteration_type}")
+  rows[3] <- glue("datatype: {datatype}")
+  rows[4] <- glue("stable_id: {stable_id}")
+  rows[5] <- glue("show_profile_in_analysis_tab: true")
+  rows[6] <- glue("profile_name: {profile_name}")
+  rows[7] <- glue("profile_description: {profile_description}")
+  rows[8] <- glue("data_filename: {data_filename}")
+  return(rows)
 }
 
-create_meta_seg <- function() {
-  return(NULL)
+create_meta_seg_generic <- function(cancer_study_identifier, genetic_alteration_type, datatype, reference_genome_id, description, data_filename) {
+  rows <- rep(NA, )
+  rows[1] <- glue("cancer_study_identifier: {cancer_study_identifier}")
+  rows[2] <- glue("genetic_alteration_type: {genetic_alteration_type}")
+  rows[3] <- glue("datatype: {datatype}")
+  rows[4] <- glue("reference_genome_id: {reference_genome_id}")
+  rows[5] <- glue("description: {description}")
+  rows[6] <- glue("data_filename: {data_filename}") 
+  return(rows)
+}
+
+create_meta_study_generic <- function(type_of_cancer, cancer_study_identifier, name, description, groups, short_name) {
+  rows <- rep(NA, 6)
+  rows[1] <- glue("type_of_cancer: {type_of_cancer}")
+  rows[2] <- glue("cancer_study_identifier: {cancer_study_identifier}")
+  rows[3] <- glue("name: {name}")
+  rows[4] <- glue("description: {description}")
+  rows[5] <- glue("groups: {groups}")
+  rows[6] <- glue("short_name: {short_name}")
+  return(rows)
+}
+
+create_meta_patient <- function(cancer_study_identifier, data_filename = "meta_clinical_patient.txt") {
+  df_file <- create_meta_clinical_generic(cancer_study_identifier = cancer_study_identifier, 
+                               genetic_alteration_type = "CLINICAL", 
+                               datatype = "PATIENT_ATTRIBUTES", 
+                               data_filename = data_filename)
+  return(df_file)
+}
+
+create_meta_sample <- function(cancer_study_identifier, data_filename = "data_clinical_sample.txt") {
+  df_file <- create_meta_clinical_generic(cancer_study_identifier = cancer_study_identifier, 
+                                          genetic_alteration_type = "CLINICAL", 
+                                          datatype = "SAMPLE_ATTRIBUTES", 
+                                          data_filename = data_filename)
+  return(df_file)
+}
+
+create_meta_timeline <- function(cancer_study_identifier, data_filename = "data_timeline.txt") {
+  df_file <- create_meta_clinical_generic(cancer_study_identifier = cancer_study_identifier, 
+                                          genetic_alteration_type = "CLINICAL", 
+                                          datatype = "TIMELINE", 
+                                          data_filename = data_filename)
+  return(df_file)
+}
+
+create_meta_matrix <- function(cancer_study_identifier, data_filename = "data_genie_matrix.txt") {
+  df_file <- create_meta_clinical_generic(cancer_study_identifier = cancer_study_identifier, 
+                                          genetic_alteration_type = "GENE_PANEL_MATRIX", 
+                                          datatype = "GENE_PANEL_MATRIX", 
+                                          data_filename = data_filename)
+  return(df_file)
+}
+
+create_meta_cna <- function(cancer_study_identifier, data_filename = "data_CNA.txt") {
+  df_file <- create_meta_genomic_generic(cancer_study_identifier, 
+                                         genetic_alteration_type = "COPY_NUMBER_ALTERATION", 
+                                         datatype = "DISCRETE", 
+                                         stable_id = "cna", 
+                                         profile_name = "Copy-number alterations", 
+                                         profile_description = "Copy-number alterations", 
+                                         data_filename)
+  
+  return(df_file)
+}
+
+create_meta_fusion <- function(cancer_study_identifier, data_filename = "data_CNA.txt") {
+  df_file <- create_meta_genomic_generic(cancer_study_identifier = cancer_study_identifier, 
+                                         genetic_alteration_type = "FUSION", 
+                                         datatype = "FUSION", 
+                                         stable_id = "fusion", 
+                                         profile_name = "Fusions", 
+                                         profile_description = "Fusions",
+                                         data_filename = data_filename)
+  return(df_file)
+}
+
+
+create_meta_maf <- function() {
+  df_file <- create_meta_genomic_generic(cancer_study_identifier = cancer_study_identifier, 
+                                         genetic_alteration_type = "MUTATION_EXTENDED", 
+                                         datatype = "MAF", 
+                                         stable_id = "mutations",
+                                         profile_name = "Mutations",
+                                         profile_description = "Mutation data from next-gen sequencing.",
+                                         data_filename = data_filename)
+  return(df_file)
+}
+
+create_meta_seg <- function(cancer_study_identifier, data_filename = "") {
+  df_file <- create_meta_seg_generic(cancer_study_identifier = cancer_study_identifier, 
+                                     genetic_alteration_type = "COPY_NUMBER_ALTERATION", 
+                                     datatype = "SEG", 
+                                     reference_genome_id = "hg19", 
+                                     description = "Segment data for the genie study", 
+                                     data_filename = data_filename) {
+    
+  return(df_file)
 }
 
 create_meta_study <- function() {
-  return(NULL)
+  df_file <- create_meta_study_generic(type_of_cancer = type_of_cancer, 
+                                       cancer_study_identifier, 
+                                       name, 
+                                       description, 
+                                       groups, 
+                                       short_name)
+  return(df_file)
+}
+
+#' Map function names to implementations.
+#' 
+#' @param file_type string representing cBio file type labels
+#' @param file_types vector of string representing all valid file type labels
+#' @return object representing function
+get_fxn <- function(file_type, 
+                    file_types = c("patient", "sample", "timeline", "cna", "matrix", "maf", "seg", "study")) {
+  
+  map_fxn_str <- list()
+  names_fxn <- paste0("create_meta_", file_types)
+  
+  for (name_fxn in names_fxn) {
+    map_fxn_str[[name_fxn]] <- get(name_fxn)
+  }
+  
+  return(map_fxn_str[[glue("create_meta_{file_type}")]])
 }
 
 create_meta <- function(file_type) {
   
-  df_type <- NULL
-  
-  if (file_type == "study") {
-    df_file <- create_meta_study()
-  } else if (file_type == "seg") {
-    df_file <- create_meta_seg()
-  } else if (is.element(file_type, c("patient", "sample", "timeline"))) {
-    df_file <- create_meta_clinical()
-  } else if (is.element(file_type, c("cna", "fusion", "matrix", "maf"))) {
-    df_file <- create_meta_genomic()
-  }
-  
+  fxn <- get_fxn(file_type)
+  df_type <- fxn()
   return(df_type)
 }
 
@@ -113,7 +239,7 @@ for (i in 1:length(filenames)) {
                                           parent_id = synid_folder_output, 
                                           file_name = filename, 
                                           prov_name = "cBioPortal meta file", 
-                                          prov_desc = "", 
+                                          prov_desc = "cBioPortal meta files for annotating provided cBioPortal data files", 
                                           prov_used = NA, 
                                           prov_exec = "https://github.com/Sage-Bionetworks/genie-erbb2-cbio/blob/develop/create_meta.R")
                         }
